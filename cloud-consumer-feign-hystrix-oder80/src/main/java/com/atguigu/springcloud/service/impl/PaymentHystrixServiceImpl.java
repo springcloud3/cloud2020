adc78b0ef0f840c2141ad19ea78600f2f69a6c5e
springcloud3 <623631000@qq.com>
2020-10-12 18:27:26 +0800

    test

---
+++ cloud-consumer-feign-hystrix-oder80/src/main/java/com/atguigu/springcloud/service/impl/PaymentHystrixServiceImpl.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.PaymentHystrixService;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentHystrixServiceImpl implements PaymentHystrixService {
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.dao;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+
+/**
+ * @Author chenzhaoxing
+ * @Date
+ * @Version 1.0
+ */
+@Mapper
+public interface PaymentDao {
+    public  int create(Payment payment);
+
+    public  Payment getPaymentById(@Param("id") Long id);
+}
